f466310de73103e98fccb23d90ecd267e431ce1c
ErauQsPhan <mrsplol102@gmail.com>
2024-04-27 16:51:06 +0700

    express

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+  res.send("<h1>Home page</h1>");
+});
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact page</h1>");
+});
+app.get("/about", (req, res) => {
+  res.send("<h1>About page</h1>");
 });
 
 app.listen(port, () => {
-  console.log(`Server Running on port ${port}`);
+  console.log(`Server running on the port ${port}`);
 });
